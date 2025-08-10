3fb74aaf9c9a6c3310e207f30e4dabace9d491d6
Your Name <karan2229sharma@gmail.com>
2025-08-10 10:14:52 +0530

    Crawling upto depth

--- library/crawl.h
+++ library/crawl.h
@@ -4,23 +4,23 @@
 #include <filesystem>
 #include <ctime>
 #include <cstdio>
+#include <cstring>
 #include "../../strings/hepler.h"
 #include "../../HashMapGeneric/library/genericHash.h"
 #include "./path.h"
 #include "./isHTML.h"
 #include "../../Genericlists/library/operations.h"
 
-using namespace std;
-
 class Crawl
 {
+public:
     char *seed_url;
     char *target_Dir;
     int crawling_Depth;
     char downloaded_Path[200];
-    HashMap<char *, char *> *allURL = new HashMap<char *, char *>(30);
+    List<char*> visited;
+    HashMap<char *, char *> *htmlPagePath = new HashMap<char *, char *>(30);
 
-public:
     Crawl(char *url, char *dir, int depth)
     {
         seed_url = new char[my_strlen(url) + 1];
@@ -28,6 +28,39 @@ public:
         crawling_Depth = depth;
         my_strcpy(seed_url, url);
         my_strcpy(target_Dir, dir);
+    }
+
+    char *normalizeURL(const char *url)
+    {
+        int len = my_strlen((char *)url);
+        char *result = new char[len + 1];
+        int i = 0, j = 0;
+        if (my_strncmp((char *)url, (char *)"https://", 8) == 0)
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                result[j++] = url[i++];
+            }
+        }
+
+        char lastChar = '\0';
+        while (url[i] != '\0')
+        {
+            if (url[i] == '/' && lastChar == '/')
+            {
+                i++;
+                continue;
+            }
+            result[j++] = url[i];
+            lastChar = url[i];
+            i++;
+        }
+
+        if (j > 1 && result[j - 1] == '/')
+            j--;
+
+        result[j] = '\0';
+        return result;
     }
 
     void downloadPage(char *url, char *dir, int depth)
@@ -38,14 +71,16 @@ public:
         }
 
         char downloadPath[200] = "../public/";
-        clock_t timeStamp = time(0);
+        time_t timeStamp = time(0);
         char timeCharArray[30];
         sprintf(timeCharArray, "%ld", timeStamp);
 
         my_strcat(downloadPath, dir);
         my_strcat(downloadPath, (char *)"/");
         my_strcat(downloadPath, timeCharArray);
-        my_strcat(downloadPath, (char *)"downloaded_page.html");
+        my_strcat(downloadPath, (char *)"_downloaded_page.html");
+
+        htmlPagePath->addInTable(url, downloadPath);
         my_strcpy(downloaded_Path, downloadPath);
 
         char command[300] = "wget -O ";
@@ -56,108 +91,155 @@ public:
         int result = system(command);
         if (result == 0)
         {
-            cout << "Successfull" << "\n";
+            cout << "Downloaded: " << url << "\n";
         }
         else
         {
-            cout << "Failed" << url << "\n";
+            cout << "Failed: " << url << "\n";
         }
     }
 
-    List<char *> extractLinks(char *fileName, char *currentURL)
-    {
-        ifstream in(fileName);
-        char c;
-        char startsWith[] = "href";
-        int index = 0;
-        List<char *> myList;
-        // cout<<"aagya\n";
-        while (in.get(c))
-        {
-            if (startsWith[index] != '\0' && startsWith[index] == c)
-            {
-                index++;
-            }
-            else if (startsWith[index] == '\0')
-            {
-                while (in.get(c))
-                {
-                    if (c == '"' || c == '\'')
-                        break;
-                }
-
-                char *url = new char[150];
-                int urlIndex = 0;
-                while (in.get(c))
-                {
-                    if (c == '"' || c == '\'')
-                        break;
-                    url[urlIndex++] = c;
-                }
-                url[urlIndex] = '\0';
-
-                if (isHtml(url))
-                {
-                    // cout << url << "\n\n";
-                    myList.addAtLast(url);
-                }
-                if (isHtml(url) && !isStartsWith(url, "https"))
-                {
-                    char *fullUrl = new char[150];
-                    fullUrl[0] = '\0';
-                    my_strcat(fullUrl, currentURL);
-                    if (url[0] == '#')
-                    {
-                        my_strcat(fullUrl, (char *)"/");
-                    }
-                    my_strcat(fullUrl, url);
-                    myList.addAtLast(fullUrl);
-                }
-                index = 0;
-            }
-            else
-            {
-                index = 0;
-            }
-        }
-        return myList;
-    }
-
-    void DFS(char *currentURL, int depthLeft)
-    {
-        if (depthLeft == 0)
-            return;
-        if (allURL->search(currentURL))
-            return;
-        cout << "hello";
-        allURL->addInTable(currentURL, (char *)"1");
-        // allURL->display();
-
-        downloadPage(currentURL, target_Dir, crawling_Depth - depthLeft + 1);
-        cout<<"After download\n";
-        List<char *> links = extractLinks(downloaded_Path, currentURL);
-        // links.display();
-        cout<<"Before DFS\n";
-        Node<char *> *temp = links.head;
-        while (temp)
-        {
-            DFS(temp->val, depthLeft - 1);
-            temp = temp->next;
-        }
-    }
+    void extractLinks(char *currentURL, int depthLeft);
+    
+    void DFS(char *currentURL, int depthLeft);
 
     void startCrawling()
     {
         DFS(seed_url, crawling_Depth);
-        cout << "here\n\n";
     }
 
     ~Crawl()
     {
-        delete seed_url;
-        delete target_Dir;
+        delete[] seed_url;
+        delete[] target_Dir;
+        delete htmlPagePath;
     }
 };
+
+void Crawl::DFS(char *currentURL, int depthLeft)
+{
+    if (depthLeft <= 0)
+        return;
+
+    char *normalizedURL = normalizeURL(currentURL);
+    bool alreadyVisited = false;
+    Node<char*> *cur = visited.head;
+    while (cur)
+    {
+        if (strcmp(cur->val, normalizedURL) == 0)
+        {
+            alreadyVisited = true;
+            break;
+        }
+        cur = cur->next;
+    }
+    if (alreadyVisited)
+    {
+        delete[] normalizedURL;
+        return;
+    }
+
+    visited.addAtStart(normalizedURL); 
+    downloadPage(normalizedURL, target_Dir, crawling_Depth - depthLeft + 1);
+    Node<Pair<char*, char*>>* result = htmlPagePath -> search(normalizedURL);
+    cout<<result -> val.val;
+    extractLinks(normalizedURL, depthLeft - 1);
+}
+
+void Crawl::extractLinks(char *currentURL, int depthLeft)
+{
+    if (depthLeft <= 0) return;
+
+    Node<Pair<char *, char *>> *result = htmlPagePath->search(currentURL);
+    if (!result) return;
+
+    char *fileName = result->val.val;
+    std::ifstream in(fileName);
+    if (!in.is_open())
+        return;
+
+    char c;
+    const char startsWith[] = "href";
+    int index = 0;
+    char *arr[100];
+    int ind = 0;
+
+    while (in.get(c))
+    {
+        if (startsWith[index] != '\0' && startsWith[index] == c)
+        {
+            index++;
+        }
+        else if (startsWith[index] == '\0')
+        {
+            while (in.get(c))
+            {
+                if (c == '"' || c == '\'')
+                    break;
+            }
+
+            char *url = new char[200];
+            int urlIndex = 0;
+            while (in.get(c))
+            {
+                if (c == '"' || c == '\'')
+                    break;
+                url[urlIndex++] = c;
+            }
+            url[urlIndex] = '\0';
+
+            if (isHtml(url) && isStartsWith(url, "https"))
+            {
+                if (ind < 100)
+                    arr[ind++] = url;
+                else
+                    delete[] url;
+            }
+            else if (isHtml(url) && url[0] != '#')
+            {
+                char *fullUrl = new char[300];
+                fullUrl[0] = '\0';
+
+                char *slashPos = strrchr(currentURL, '/');
+                if (slashPos)
+                {
+                    int baseLen = slashPos - currentURL + 1;
+                    strncpy(fullUrl, currentURL, baseLen);
+                    fullUrl[baseLen] = '\0';
+                }
+                else
+                {
+                    my_strcpy(fullUrl, currentURL);
+                    my_strcat(fullUrl, "/");
+                }
+
+                my_strcat(fullUrl, url);
+                delete[] url;
+                if (ind < 100)
+                    arr[ind++] = fullUrl;
+                else
+                    delete[] fullUrl;
+            }
+            else
+            {
+                delete[] url;
+            }
+
+            index = 0;
+            continue;
+        }
+        else
+        {
+            index = 0;
+        }
+    }
+
+    for (int i = 0; i < ind; i++)
+    {
+        DFS(arr[i], depthLeft);
+        delete[] arr[i];
+    }
+}
 
 // #pragma once
 // #include <iostream>
@@ -191,6 +273,37 @@ public:
 //         my_strcpy(target_Dir, dir);
 //     }
 
+//     char *normalizeURL(const char *url)
+//     {
+//         int len = my_strlen((char *)url);
+//         char *result = new char[len + 1];
+//         int i = 0, j = 0;
+//         if (my_strncmp((char *)url, (char *)"https://", 8) == 0)
+//         {
+//             for (int k = 0; k < 8; k++)
+//             {
+//                 result[j++] = url[i++];
+//             }
+//         }
+
+//         char lastChar = '\0';
+//         while (url[i] != '\0')
+//         {
+//             if (url[i] == '/' && lastChar == '/')
+//             {
+//                 i++;
+//                 continue;
+//             }
+
+//             result[j++] = url[i];
+//             lastChar = url[i];
+//             i++;
+//         }
+
+//         result[j] = '\0';
+//         return result;
+//     }
+
 //     void downloadPage(char *url, char *dir, int depth)
 //     {
 //         if (!check_Path(target_Dir))
@@ -215,21 +328,62 @@ public:
 //         my_strcat(command, url);
 
 //         int result = system(command);
-//         if (result == 0) {
-//             cout << "Successfull" <<"\n";
-//         } else {
-//             cout << "Failed"<<url<<"\n";
+//         if (result == 0)
+//         {
+//             cout << "Successfull" << "\n";
+//         }
+//         else
+//         {
+//             cout << "Failed" << url << "\n";
 //         }
 //     }
 
-//     List<char *> extractLinks(char *fileName)
+//     bool isRootRelative(const char *url)
 //     {
+//         return url[0] == '/';
+//     }
+
+//     void handleRootRelative(char *currentURL,char *relativeUrl, char *fullUrl)
+//     {
+//         int index = my_strstr(currentURL, (char*)"://");
+//         cout<<index;
+//         if (index == -1)
+//         {
+//             fullUrl[0] = '\0';
+//             return;
+//         }
+//         int start = index + 3;
+//         int slashPos = -1;
+//         for (int i = start; currentURL[i] != '\0'; i++)
+//         {
+//             if (currentURL[i] == '/')
+//             {
+//                 slashPos = i;
+//                 break;
+//             }
+//         }
+
+//         if (slashPos != -1)
+//         {
+//             strncpy(fullUrl, currentURL, slashPos);
+//             fullUrl[slashPos] = '\0';
+//         }
+//         else
+//         {
+//             my_strcpy(fullUrl, currentURL);
+//         }
+//         my_strcat(fullUrl, relativeUrl);
+//     }
+
+//     void extractLinks(const char *fileName, char *currentURL, int depthLeft)
+//     {
+
+//         cout<< "extractLinks\n\n\n\n";
 //         ifstream in(fileName);
 //         char c;
 //         char startsWith[] = "href";
 //         int index = 0;
-//         List<char *> myList;
-//         // cout<<"aagya\n";
+
 //         while (in.get(c))
 //         {
 //             if (startsWith[index] != '\0' && startsWith[index] == c)
@@ -244,8 +398,9 @@ public:
 //                         break;
 //                 }
 
-//                 char *url = new char[150];
+//                 char *url = new char[200];
 //                 int urlIndex = 0;
+
 //                 while (in.get(c))
 //                 {
 //                     if (c == '"' || c == '\'')
@@ -253,46 +408,57 @@ public:
 //                     url[urlIndex++] = c;
 //                 }
 //                 url[urlIndex] = '\0';
-//                 if(isStartsWith(url,"https"))
-//                 {
 
-//                 }
-//                 if (isHtml(url))
+//                 if (isHtml(url) && isStartsWith(url, "https"))
 //                 {
-//                     cout<<url<<"\n\n\n";
-//                     myList.addAtLast(url);
+//                     DFS(url, depthLeft - 1);
 //                 }
+//                 // else if (isHtml(url) && url[0] != '#')
+//                 // {
+//                 //     char *fullUrl = new char[150];
+//                 //     fullUrl[0] = '\0';
+//                 //     my_strcat(fullUrl, currentURL);
+//                 //     my_strcat(fullUrl, url);
+//                 //     DFS(fullUrl, depthLeft - 1);
+//                 // }
+//                 else if (isHtml(url) && url[0] != '#')
+//                 {
+//                     char fullUrl[300];
+//                     if (isRootRelative(url))
+//                     {
+//                         handleRootRelative(currentURL, url, fullUrl);
+//                     }
+//                     else
+//                     {
+//                         strcpy(fullUrl, url);
+//                     }
+//                     DFS(fullUrl, depthLeft - 1);
+//                 }
+
 //                 index = 0;
+//                 continue;
 //             }
 //             else
 //             {
 //                 index = 0;
 //             }
 //         }
-//         return myList;
 //     }
 
 //     void DFS(char *currentURL, int depthLeft)
 //     {
 //         if (depthLeft == 0)
 //             return;
-//         if (allURL->search(currentURL))
+//         char *normalizedURL = normalizeURL(currentURL);
+//         if (allURL->search(normalizedURL))
 //             return;
-//         allURL->addInTable(currentURL, (char *)"1");
-//         allURL->display();
-
-//         downloadPage(currentURL, target_Dir, crawling_Depth - depthLeft + 1);
-//         // cout<<"After download\n";
-//         List<char *> links = extractLinks(downloaded_Path);
-//         // links.display();
-//         // cout<<"Before DFS\n";
-//         Node<char *> *temp = links.head;
-//         while (temp)
-//         {
-//             DFS(temp->val, depthLeft - 1);
-//             temp = temp->next;
-//         }
-
+//         allURL->addInTable(normalizedURL, (char *)"1");
+//         char* tempPath;
+//         my_strcpy(tempPath, downloaded_Path);
+//         downloadPage(normalizedURL, target_Dir, crawling_Depth - depthLeft + 1);
+//         extractWordsString(downloaded_Path);
+//         cout<<"lknvu\n\n\n";
+//         extractLinks(tempPath, normalizedURL, depthLeft);
 //     }
 
 //     void startCrawling()
--- library/path.h
+++ library/path.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <filesystem>
-#include <iostream> 
+#include <iostream>
 #include <fstream>
 #include "../../strings/hepler.h"
+#include "../../HashMapGeneric/library/genericHash.h"
 #include "./isHTML.h"
 using namespace std;
 namespace fs = std::filesystem;
@@ -26,52 +27,95 @@ bool check_Path(char *path)
     }
 }
 
-void create_Dir(char* name){
+void create_Dir(char *name)
+{
     char fullPath[30] = "../public/";
     my_strcat(fullPath, name);
-    cout<<fullPath<<endl;
-    if (fs::create_directory(fullPath)) {
-        cout<<"Directory created successfully";
-    } else {
+    if (fs::create_directory(fullPath))
+    {
+        cout << "Directory created successfully";
+    }
+    else
+    {
         cout << "Directory creation failed or already exists." << endl;
     }
 }
+void extractWordsString(const char *filePath)
+{
+    HashMap<char *, int> *wordsFrequency = new HashMap<char *, int>(30);
+    fstream sWord(filePath);
+    if (!sWord.is_open())
+    {
+        cout << "Failed to open file" << endl;
+        return;
+    }
 
-// void getLinks(char* fileName, int depth, char* target_Dir){
-//     fstream in(fileName);
-//     char c;
-//     char startsWith[] = "href";
-//     int index = 0;
-//     char urls[10][150];  
-//     int urlCount = 0;
+    char c;
+    bool inTag = false;
+    bool inStyle = false;
+    int TAG_BUFFER_SIZE = 10;
+    char tagBuffer[TAG_BUFFER_SIZE];
+    int tagIndex = 0;
 
-//     while (in.get(c)) {
-//         if (startsWith[index] != '\0' && startsWith[index] == c) {
-//             index++;
-//         }
-//         else if (startsWith[index] == '\0') {
-//             while (in.get(c)) {
-//                 if (c == '"' || c == '\'')
-//                     break;
-//             }
+    char word[100];
+    int wordIndex = 0;
 
-//             char url[150];
-//             int urlIndex = 0;
-//             while (in.get(c)) {
-//                 if (c == '"' || c == '\'')
-//                     break; 
-//                 url[urlIndex++] = c;
-//             }
-//             url[urlIndex] = '\0';
-//             if(isHtml(url)){
-//                 cout<<url<<endl;
-//                 my_strcpy(urls[urlCount], url);
-//                 urlCount++;
-//             }
-//             index = 0; 
-//         }
-//         else {
-//             index = 0;
-//         }
-//     }
-// }
+    while (sWord.get(c))
+    {
+        if (c == '<')
+        {
+            inTag = true;
+            tagIndex = 0;
+            tagBuffer[tagIndex++] = c;
+        }
+        else if (inTag)
+        {
+            if (tagIndex < TAG_BUFFER_SIZE - 1)
+            {
+                tagBuffer[tagIndex++] = c;
+            }
+
+            if (c == '>')
+            {
+                tagBuffer[tagIndex] = '\0';
+                if (isOpeningTag(tagBuffer))
+                {
+                    inStyle = true;
+                }
+                else if (isClosingTag(tagBuffer))
+                {
+                    inStyle = false;
+                }
+                inTag = false;
+            }
+        }
+        else if (!inStyle && !inTag)
+        {
+            int count = 1;
+            if (c == ' ' || c == '\n')
+            {
+                word[wordIndex] = '\0';
+                lowerCase(word);
+                wordIndex = 0;
+                Node<Pair<char *, int>> *result = wordsFrequency->search(word);
+                if (result)
+                    count = result->val.val + 1;
+                char *normalizedWord = normalizeWord(word);
+                if ((normalizedWord[0] != '\0' && normalizedWord[1] != '\0') && !isGrammarWord(normalizedWord))
+                {
+                    my_strcpy(word, normalizedWord);
+                    wordsFrequency->addInTable(word, count);
+                }
+            }
+            else
+            {
+                word[wordIndex] = c;
+                wordIndex++;
+            }
+        }
+    }
+    wordsFrequency->display();
+    char *frequentWord = wordsFrequency->getFrequent();
+    cout << "frequent word is -> " << frequentWord << "\n\n";
+    // delete []wordsFrequency;
+}
